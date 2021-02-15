49b1dba82fe22863d883f6bb78408bdd3f5fa467
Oskar Mazerath <moskar.drummer@gmail.com>
2021-02-15 12:22:05 +0200

    debug ci 2

--- test/prop_ebpf.erl
+++ test/prop_ebpf.erl
@@ -33,7 +33,7 @@ prop_kern_exit_returns_given_value() ->
         begin
             Data = <<1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16>>,
             {ok, Prog} = ebpf_user:load(
-                xdp,
+                socket_filter,
                 ebpf_asm:assemble([ebpf_kern:mov64_imm(0, Val), ebpf_kern:exit_insn()])
             ),
             {ok, Val, Data, _Duration1} = ebpf_user:test_program(Prog, 1, Data, byte_size(Data)),
