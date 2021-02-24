6f30c290e11574c3f22f93b3f439ba2def2969a1
Oskar Mazerath <moskar.drummer@gmail.com>
2021-02-24 13:50:36 +0200

    Update examples/cf_ttl.erl

--- examples/cf_ttl.erl
+++ examples/cf_ttl.erl
@@ -10,60 +10,54 @@
 
 -export([attach_ttl_bpf/1, read_ttl_bpf/1]).
 
--spec attach_ttl_bpf(integer()) -> {'ok', ebpf_user:bpf_map()}.
-attach_ttl_bpf(SockFd) ->
-    {ok, Map} = ebpf_user:create_map(hash, 4, 8, 4, 0),
-    MapFd = ebpf_user:fd(Map),
+-spec attach_ttl_bpf(socket:socket()) -> {'ok', ebpf_maps:ebpf_map()}.
+attach_ttl_bpf(Sock) ->
+    {ok, Map} = ebpf_maps:new(hash, 4, 8, 4),
+    MapFd = ebpf_maps:fd(Map),
     Instructions = lists:flatten([
-        ebpf_kern:ldx_mem(w, 0, 1, 16),
-        ebpf_kern:jmp64_imm(eq, 0, 16#86DD0000, 3),
-        ebpf_kern:mov64_reg(6, 1),
+        ebpf_kern:ldx_mem(w, r0, r1, 16),
+        ebpf_kern:jmp64_imm(eq, r0, 16#86DD, 3),
+        ebpf_kern:mov64_reg(r6, r1),
         ebpf_kern:ld_abs(b, -16#100000 + 8),
         ebpf_kern:jmp_a(2),
-        ebpf_kern:mov64_reg(6, 1),
+        ebpf_kern:mov64_reg(r6, r1),
         ebpf_kern:ld_abs(b, -16#100000 + 7),
-        ebpf_kern:stx_mem(w, 10, 0, -4),
-        ebpf_kern:mov64_reg(2, 10),
-        ebpf_kern:alu64_imm(add, 2, -4),
-        ebpf_kern:ld_map_fd(1, MapFd),
+        ebpf_kern:stx_mem(w, r10, r0, -4),
+        ebpf_kern:mov64_reg(r2, r10),
+        ebpf_kern:alu64_imm(add, r2, -4),
+        ebpf_kern:ld_map_fd(r1, MapFd),
         ebpf_kern:call_helper(map_lookup_elem),
-        ebpf_kern:jmp64_imm(eq, 0, 0, 3),
-        ebpf_kern:mov64_imm(1, 1),
-        ebpf_kern:stx_xadd(dw, 0, 1, 0),
+        ebpf_kern:jmp64_imm(eq, r0, 0, 3),
+        ebpf_kern:mov64_imm(r1, 1),
+        ebpf_kern:stx_xadd(dw, r0, r1, 0),
         ebpf_kern:jmp_a(9),
-        ebpf_kern:ld_map_fd(1, MapFd),
-        ebpf_kern:mov64_reg(2, 10),
-        ebpf_kern:alu64_imm(add, 2, -4),
-        ebpf_kern:st_mem(dw, 10, -16, 1),
-        ebpf_kern:mov64_reg(3, 10),
-        ebpf_kern:alu64_imm(add, 3, -16),
-        ebpf_kern:mov64_imm(4, 0),
+        ebpf_kern:ld_map_fd(r1, MapFd),
+        ebpf_kern:mov64_reg(r2, r10),
+        ebpf_kern:alu64_imm(add, r2, -4),
+        ebpf_kern:st_mem(dw, r10, -16, 1),
+        ebpf_kern:mov64_reg(r3, r10),
+        ebpf_kern:alu64_imm(add, r3, -16),
+        ebpf_kern:mov64_imm(r4, 0),
         ebpf_kern:call_helper(map_update_elem),
-        ebpf_kern:mov64_imm(0, -1),
+        ebpf_kern:mov64_imm(r0, -1),
         ebpf_kern:exit_insn()
     ]),
     {ok, Prog} = ebpf_user:load(socket_filter, ebpf_asm:assemble(Instructions)),
-    ok = ebpf_user:attach_socket_filter(SockFd, Prog),
+    ok = ebpf_user:attach_socket_filter(Sock, Prog),
     {ok, Map}.
 
--spec read_ttl_bpf(ebpf_user:bpf_map()) -> 'empty' | {'ok', non_neg_integer()} | {error, term()}.
+-spec read_ttl_bpf(ebpf_maps:ebpf_map()) -> 'empty' | {'ok', non_neg_integer()} | {error, term()}.
 read_ttl_bpf(Map) ->
-    read_ttl_bpf(Map, <<0:32>>, empty).
+    read_ttl_bpf(ebpf_maps:next(ebpf_maps:iterator(Map)), error).
 
--spec read_ttl_bpf(ebpf_user:bpf_map(), binary(), 'empty' | non_neg_integer()) ->
-    'empty' | {'ok', non_neg_integer()} | {error, term()}.
-read_ttl_bpf(Map, Key, Min) ->
-    case ebpf_user:get_map_next_key(Map, Key) of
-        {error, enoent} ->
-            Min;
-        {ok, <<Ttl:32/little-unsigned-integer>>} ->
-            io:format("~p~n~n", [Ttl]),
-            if
-                Ttl > 128 -> {ok, min(Min, 255 - Ttl)};
-                Ttl > 64 -> {ok, min(Min, 128 - Ttl)};
-                Ttl > 32 -> {ok, min(Min, 64 - Ttl)};
-                true -> {ok, min(Min, 32 - Ttl)}
-            end;
-        Other ->
-            {error, Other}
+-spec read_ttl_bpf('none' | {<<_:32>>, _, _}, _) -> {'ok', _}.
+read_ttl_bpf(none, Min) ->
+    {ok, Min};
+read_ttl_bpf({Key, _Value, Iterator}, Min) ->
+    <<Ttl:32/little-unsigned-integer>> = Key,
+    if
+        Ttl > 128 -> read_ttl_bpf(ebpf_maps:next(Iterator), min(Min, 255 - Ttl));
+        Ttl > 64 -> read_ttl_bpf(ebpf_maps:next(Iterator), min(Min, 128 - Ttl));
+        Ttl > 32 -> read_ttl_bpf(ebpf_maps:next(Iterator), min(Min, 64 - Ttl));
+        true -> read_ttl_bpf(ebpf_maps:next(Iterator), min(Min, 32 - Ttl))
     end.
